7e5f679317462b03783ae3a5230239dd7e752e47
juliamonsores <juliamonsores808@gmail.com>
2021-12-03 09:30:15 -0300

    javascript
    javascript botão

---
+++ index.java
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<head>        
+    <title>  testando java  </title>
+</head>
+<html> 
+<body> 
+
+
+<h2> surpresa </h2> 
+<p> agora </p> 
+<p id= "demo" style= "display:none" > apareceu </p> 
+<button type = "button" onclick="document.getElementById('demo').style.display='block'"> clica aqui </button> 
+
+
+
+
+
+
+
+
+</body> 
+</html> 
