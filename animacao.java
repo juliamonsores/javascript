74a7fa3ad518c9b5f26c478f85a1bc35625f09b1
juliamonsores <juliamonsores808@gmail.com>
2021-12-03 09:37:08 -0300

    javascript com jquery
    pequena animação com javascript e jquery

---
+++ animacao.java
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+
+<html> 
+<head>
+<title> Jquery</title>
+<meta charset="UTF-8">
+<style> 
+body {background-color: grey; 
+color: black;
+text-align: center;
+font-size: 40px;
+padding-top: 200px;
+font-family: helvetica;
+}
+
+
+</style>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script>   $(document).ready(function(){$("#p1").mousedown(function(){alert ("Isso acontece, maneiro ne");});});</script>
+</head>
+
+
+
+
+<body>
+
+
+<p id="p1"> clique aqui para ver oque vai acontecer </p> 
+
+
+
+</body>
+
+
+
+
+
+</html>
